94c2a2d0ad0c46c79faa62ed633a17f14d6a96aa
juan <jgama12569@gmail.com>
2019-11-18 11:12:49 -0600

    velocista

--- el_velocista_ver1.0/el_velocista_ver1.0.ino
+++ el_velocista_ver1.0/el_velocista_ver1.0.ino
@@ -17,16 +17,18 @@ void setup() {
   pinMode(24, INPUT);
   pinMode(26, INPUT);
   pinMode(28, INPUT);
+  pinMode(7,OUTPUT);
+  pinMode(4,OUTPUT);
   Serial.begin(9600);
 }
 void loop() {
-  IR[0] = digitalRead(22);
+  IR[0] = digitalRead(22);//se leen los sensores
   IR[1] = digitalRead(24);
   IR[2] = digitalRead(26);
   IR[3] = digitalRead(28);
   p =  -2 * IR[0] -   IR[1] + IR[2] + 2 * IR[3]; //en la variable p(proporcional) se unifican todas las lecturas de los sensores
   i = i + p;//en la variable i (integral) se van sumando todos los valores de p
-  d = p - p_old;//en la variable d(direccional)
+  d = p - p_old;//en la variable d(direccional) se saca la diferencia del p pasado y el actual
   p_old = p;//actualiza la variable p_old para el proximo ciclo
   if ((p * i) < 0) i = 0; // limpia el valor de i
   u = kp * p + ki * i + kd * d; //calcula el pwm que le tiene que dar a las llantas
@@ -35,5 +37,12 @@ void loop() {
   Serial.println(u);
 }
 void moverse(float valorder, float valorizq) {
-
+  L = constrain(L, -255, 255); // hace que el pwm no se exceda de 255
+  R = constrain(R, -255, 255);// hace que el pwm no se exceda de 255
+//ruedaizq
+  digitalWrite(7, 0);
+  analogWrite(6, abs(L));//solo un pin se declara como analogico porque solo se necesia ir hacia adelante
+//rueda derecha
+  digitalWrite(4, 0);
+  analogWrite(5, abs(R));//solo un pin se declara como analogico porque solo se necesia ir hacia adelante
 }
